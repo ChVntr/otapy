ce9d6a2b311f5fb10668b84f36f204296af806a0
ChVntr <cahiosantos11@gmail.com>
2024-12-05 13:33:04 -0300

    Atualizar o ota.py

--- ota.py
+++ ota.py
@@ -498,6 +498,7 @@ if verifyos() == 1:
 
     upfile = open('mpv\\updater.bat', 'r')
     lastline = (upfile.readlines()[-1])
+    print(lastline)
     if lastline == 'timeout 5':
         print('timeout = true')
     upfile.close()
