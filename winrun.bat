c0a9a0299c3da3627d734594d71399c7a7271ff2
ChVntr <cahiosantos11@gmail.com>
2024-12-06 12:19:49 -0300

    Add files via upload

--- winrun.bat
+++ winrun.bat
@@ -1,22 +1,70 @@
 @echo off
 
 
+
+
+
+:: checando se tem python instalado
+
+(python -V) || (
+    echo PYTHON NAO ENCONTRADO
+    echo.
+    echo INSTALE PYTHON E REINICIE O PROGRAMA PARA CONTINUAR
+    echo.
+    pause
+    exit
+) 
+cls
+
+
+
+
+
+:: baixando o script em python
+
 set pylink=https://raw.githubusercontent.com/ChVntr/otapy/refs/heads/main/ota.py
 
 if exist otapy\ (
     set otapy=1
 ) else (
-    set otapy=0
     echo BAIXANDO ARQUIVOS NECESSARIOS...
     echo.
     mkdir otapy
     cd otapy
     powershell Invoke-WebRequest %pylink% -OutFile ota.py
+    cd ..
+    set otapy=1
+) 
+cls
+
+
+
+
+
+
+
+:: instalando player se não tiver um instalado
+
+set player=1
+
+(mpv -V) || (vlc -V) || (otapy\mpv\mpv.exe -V) || (set player=0)
+cls
+
+if %player% == 0 (
+    echo INSTALANDO REPRODUTOR DE VIDEO...
+    cd otapy
     mkdir mpv
     powershell Invoke-WebRequest https://github.com/ChVntr/otapy/releases/download/depend/mpv.zip -OutFile mpv.zip
     powershell Expand-Archive mpv.zip -DestinationPath mpv
     del mpv.zip
-)
+    cd ..
+) 
+cls
+
+
+
+
+
 
 
 if %otapy% == 1 (
