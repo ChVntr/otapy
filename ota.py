b30fea54528ad6941b1135af215fa57c87e862ad
ChVntr <cahiosantos11@gmail.com>
2024-12-17 17:28:58 -0300

    Update ota.py

--- ota.py
+++ ota.py
@@ -140,13 +140,14 @@ def proximo(sopa):
     
     titulo = titulo.replace('\\u00bd', '½')
     titulo = titulo.replace('&quot;', '')
+    titulo = titulo.replace('\\', '')
 
     print(str(''.join(['BUSCANDO ANIME:\n', titulo, '\nEP:\n', ep, '\n\n'])))
 
     titulo = titulo.replace('u2606', ' ')
     titulo = titulo.replace('u2605', ' ')
     titulo = titulo.replace('½', '')
-    titulo = titulo.replace('\\', '')
+
     titulo = titulo.replace('    ', ' ')
     titulo = titulo.replace('   ', ' ')
     titulo = titulo.replace('  ', ' ')
